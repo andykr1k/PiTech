393ed8021bfcf5888882e451b7adc9476fef91ee
issy16 <ichen064@ucr.edu>
2024-11-08 11:52:49 -0800

    add container, slot, and grid class

--- src/models/container.py
+++
@@ -1,8 +0,0 @@
-class Container:
-    # represent each container, include weight, position, name
-    def __init__(self, name, weight):
-        self.name = name
-        self.weight = weight
-    
-    def __repr__(self):
-        return f"Container(name={self.name}, weight={self.weight})"
