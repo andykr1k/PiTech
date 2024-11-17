c98e9f221df6cdb38f6eb1c25ea4041f80dd23c0
andykr1k <69021102+andykr1k@users.noreply.github.com>
2024-11-17 11:38:25 -0800

    Grid Skeleton Created - Needs Data to Continue work

--- GUI/Screens/TestPage.py
+++ GUI/Screens/TestPage.py
@@ -1,10 +1,11 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QPushButton
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
 class TestPage(QWidget):
-    def __init__(self):
+    def __init__(self, stacked_widget):
         super().__init__()
+        self.stacked_widget = stacked_widget
         self.initUI()
 
     def initUI(self):
@@ -21,4 +22,25 @@ class TestPage(QWidget):
 
         main_layout.addLayout(layout)
 
+        self.continueButton = QPushButton("Continue")
+        self.continueButton.setFont(QFont("Arial", 14))
+        self.continueButton.setStyleSheet(
+            """
+            QPushButton {
+                background-color: #2F27CE;
+                color: white;
+                border-radius: 10px;
+                padding: 10px;
+            }
+            QPushButton:hover {
+                background-color: #2F27CE;
+            }
+            """
+        )
+        self.continueButton.clicked.connect(self.goToNextPage)
+        layout.addWidget(self.continueButton, alignment=Qt.AlignCenter)
+
         self.setLayout(main_layout)
+
+    def goToNextPage(self):
+        self.stacked_widget.setCurrentIndex(2)
--- GUI/main.py
+++ GUI/main.py
@@ -2,13 +2,16 @@ import sys
 from PyQt5.QtWidgets import QApplication, QStackedWidget
 from Screens.HomePage import HomePage
 from Screens.TestPage import TestPage
+from Screens.OperationPage import OperationPage
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     stacked_widget = QStackedWidget()
     homePage = HomePage(stacked_widget)
-    testpage = TestPage()
+    testpage = TestPage(stacked_widget)
+    operationpage = OperationPage()
     stacked_widget.addWidget(homePage)
     stacked_widget.addWidget(testpage)
+    stacked_widget.addWidget(operationpage)
     stacked_widget.show()
     sys.exit(app.exec_())
