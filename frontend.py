54c33bf879b4ab9557e1e7e8a2d2844dec8c2f19
Cristian Arauz <carau013@ucr.edu>
2024-11-20 14:05:49 -0800

    Change location of HomePage and main

--- frontend.py
+++ frontend.py
@@ -1,14 +1,21 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QStackedWidget
-from Screens.HomePage import HomePage
-from Screens.TestPage import TestPage
+from Screens import HomePage, SignInPage
+
+class MainWidget(QStackedWidget):
+    def __init__(self):
+        super().__init__()
+        self.sign_in_page = SignInPage(self)
+        self.home_page = HomePage(self)
+        self.addWidget(self.sign_in_page)
+        self.addWidget(self.home_page)
+        
+    @property
+    def current_page(self):
+        return self.home_page
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    stacked_widget = QStackedWidget()
-    homePage = HomePage(stacked_widget)
-    testpage = TestPage()
-    stacked_widget.addWidget(homePage)
-    stacked_widget.addWidget(testpage)
-    stacked_widget.show()
+    main_widget = MainWidget()
+    main_widget.show()
     sys.exit(app.exec_())
