a01badac0339c8f01e6fd3c42bafed8bc900f0f0
andykr1k <69021102+andykr1k@users.noreply.github.com>
2024-12-10 14:46:43 -0800

    Added Build File

---
+++ build.py
@@ -0,0 +1,22 @@
+# make_exe.py
+from cx_Freeze import setup, Executable
+import sys
+
+includes = []
+excludes = []
+packages = []
+
+# Console or Win32GUI
+base = None
+if sys.platform == "win32":
+    # base = 'Console'
+    base = 'Win32GUI'
+
+filename = "main.py"
+setup(
+    name='PiTech',
+    version='0.1',
+    description='Test',
+    options={'build_exe': {'excludes': excludes,
+                           'packages': packages, 'includes': includes}},
+    executables=[Executable(filename, base=base, icon=None)])
