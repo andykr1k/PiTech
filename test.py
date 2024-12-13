9f231947b852a40dba2bc844880e9521983629f3
issy16 <ichen064@ucr.edu>
2024-12-12 21:10:34 -0800

    .

--- test.py
+++ test.py
@@ -4,22 +4,19 @@ from Backend.Classes.Pathfinder import Pathfinder
 import time
 import signal
 
-# Timeout exception class
 class TimeoutException(Exception):
     pass
 
-# Timeout handler
 def timeout_handler(signum, frame):
     raise TimeoutException
 timeout_duration = 900  # 10 minutes
-test_cases2 = [f"{i}Containers.txt" for i in range(41, 51)]  # Generate filenames dynamically
+test_cases2 = [f"{i}Containers.txt" for i in range(41, 51)] 
 
-# Iterate through balancing test cases
 for manifest_name in test_cases2:
     try:
-        # Set the timeout handler
+        
         signal.signal(signal.SIGALRM, timeout_handler)
-        signal.alarm(timeout_duration)  # Start the timeout clock
+        signal.alarm(timeout_duration) 
 
         manifest_data = upload_manifest(manifest_name)
 
@@ -30,13 +27,11 @@ for manifest_name in test_cases2:
         print(f"Balancing job selected for {manifest_name}.")
         start_time = time.time()
 
-        # Run the balance function
         balance_moves = pathfinder.balance()
 
         end_time = time.time()
         cost_time = end_time - start_time
 
-        # Display results
         print(f"\nBalance Moves for {manifest_name}:")
         for move in balance_moves:
             print(move[0])
